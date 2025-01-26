f42e45111840f4c73ada759344e1b233f6e4348a
MatejSkultety <matej.skultety.official@gmail.com>
2025-01-27 00:51:29 +0100

    feat: implemented tcpdump

--- __main__.py
+++ __main__.py
@@ -1,10 +1,16 @@
 import docker
 import subprocess
+import time
 
 client = docker.from_env()
-print(client.containers.list())
 sysdig_container = client.containers.run(
     "sysdig/sysdig",
+    stdin_open=True,
+    tty=True,
+    detach=True,
+)
+tcpdump_container = client.containers.run(
+    "tcpdump",
     stdin_open=True,
     tty=True,
     detach=True,
@@ -17,12 +23,11 @@ tested_container = client.containers.run(
 )
 print(tested_container)
 print(tested_container.name)
+
 print(subprocess.run([f"sudo sysdig -j -n 3 container.name={tested_container.name} > sysdig_output.json"], shell=True))
+print(subprocess.run([f"sudo tcpdump -E json -w tcpdump_output.pcap -i any"], shell=True))
 
-
-print(client.containers.list())
-
-
+time.sleep(10)
 for container in client.containers.list():
     container.stop()
-print("Good bye, Docker!")
+print("Good bye, PyDetective!")
