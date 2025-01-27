6500feff2ce149d17c79aae91f1d50b065c61193
MatejSkultety <matej.skultety.official@gmail.com>
2025-01-27 01:04:25 +0100

    feat: add debug print statements and improve container management in PyDetective

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import docker
 import subprocess
 import time
 
+print("PyDetective started!")
 client = docker.from_env()
 sysdig_container = client.containers.run(
     "sysdig/sysdig",
@@ -9,25 +10,29 @@ sysdig_container = client.containers.run(
     tty=True,
     detach=True,
 )
+print("PyDetective debug: Sysdig container started")
 tcpdump_container = client.containers.run(
     "tcpdump",
     stdin_open=True,
     tty=True,
     detach=True,
 )
+print("PyDetective debug: Tcpdump container started")
 tested_container = client.containers.run(
     "pandas_test",
     stdin_open=True,
     tty=True,
     detach=True,
 )
+print("PyDetective debug: Tested container started: 'pandas'")
 print(tested_container)
-print(tested_container.name)
 
-print(subprocess.run([f"sudo sysdig -j -n 3 container.name={tested_container.name} > sysdig_output.json"], shell=True))
-print(subprocess.run([f"sudo tcpdump -E json -w tcpdump_output.pcap -i any"], shell=True))
+subprocess.run([f"sudo sysdig -j -n 3 container.name={tested_container.name} > sysdig_output.json"], shell=True)
+print("PyDetective debug: Sysdig output saved to sysdig_output.json")
+subprocess.run([f"sudo tcpdump -E json -w tcpdump_output.pcap -i any"], shell=True)
+print("PyDetective debug: Tcpdump output saved to tcpdump_output.json")
 
-time.sleep(10)
+time.sleep(1)
 for container in client.containers.list():
     container.stop()
 print("Good bye, PyDetective!")
