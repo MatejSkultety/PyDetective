01f37df60f236a5add9b2b721ed2b8b6bd50f6cd
MatejSkultety <matej.skultety.official@gmail.com>
2025-05-10 23:44:55 +0200

    refactor: update main.py for testing

--- __main__.py
+++ __main__.py
@@ -1,9 +1,12 @@
 import os
 import argparse
 import sys
+import docker
 
-from src.utils import runner
-from src.analysis import static
+from src import analysis
+from src import containers
+from src import runner
+
 
 def arg_formatter():
     def formatter(prog): return argparse.HelpFormatter(
@@ -34,17 +37,26 @@ def main():
     print('-' * terminal_size.columns)
 
     if args.test:
-        runner.install_in_sandbox("purposefully-malicious")
+        # runner.install_in_sandbox("purposefully-malicious")
         print('-' * terminal_size.columns)
 
     if args.local:
-        runner.install_in_sandbox("/app/sample_package_skuty")
+        # runner.install_in_sandbox("/app/sample_package_skuty")
+
+        client = docker.from_env()
+        # runner.analyze_package(client, "sandbox/sample_malicious_package", secure_mode=False)
+        containers.build_sandbox_image(client)
+        # print(evaluation.evaluate_package("sandbox/sample_malicious_package", "out/syscalls_result.json", "out/static_result.json"))
         print('-' * terminal_size.columns)
 
     if args.dev:
-        static.scan_file_with_rules(None, "src/analysis/yara_rules/malware")
-        print('-' * terminal_size.columns)
+        # analysis.analyse_syscalls_artefacts(None)
 
+        analyzer = analysis.StaticAnalyzer("rules/static_rules")
+        analyzer.scan_directory("sandbox/sample_malicious_package", "out/static_result.json")
+
+        # client = docker.from_env()
+        # runner.analyze_package(client, "sandbox/sample_malicious_package", secure_mode=False)
 
 if __name__ == '__main__':
     main()
