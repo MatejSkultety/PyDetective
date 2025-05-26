9615493c33d192db1d14943450394e36d1211429
MatejSkultety <matej.skultety.official@gmail.com>
2025-05-26 18:21:09 +0200

    refactor: improve error handling in sample_package

--- sandbox/sample_malicious_package/src/__init__.py
+++ sandbox/sample_malicious_package/src/__init__.py
@@ -2,13 +2,19 @@ import sys
 import os
 from datetime import datetime
 from pathlib import Path
-from malicious import *
+from .malicious import *
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(SCRIPT_DIR)
 
-[f("__init__.py", True) for f in simulated_techniques]
+try:
+    [f("__init__.py", True) for f in simulated_techniques]
+except Exception as e:
+    print(f"An error occurred while executing simulated techniques: {e}")
 
-Path("/temp").mkdir(parents=True, exist_ok=True)
-with open("/temp/virus.txt", "w", encoding="utf-8") as buffer:
-    buffer.write(f"I was here at {datetime.now()} ;>")
+try:
+    Path("/temp").mkdir(parents=True, exist_ok=True)
+    with open("/temp/virus.txt", "w", encoding="utf-8") as buffer:
+        buffer.write(f"I was here at {datetime.now()} ;>")
+except Exception as e:
+    print(f"An error occurred while writing to /temp/virus.txt: {e}")
